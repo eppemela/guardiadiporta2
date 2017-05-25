60dcdedf727281273a73dff7e0967fb6d1ba696c
ing.norante <eppedema@gmail.com>
2017-05-25 19:23:57 +0200

    Basic layout template

--- Gemfile
+++ Gemfile
@@ -21,10 +21,7 @@ gem 'uglifier', '>= 1.3.0'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
-# Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
@@ -37,6 +34,10 @@ gem 'material_icons'
 gem 'rest-client'
 gem 'watir'
 gem 'awesome_print'
+gem 'jquery-rails'
+gem 'slim-rails'
+
+
 
 group :development, :test do
   gem 'pry-rails'
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,41 +1,41 @@
 class StaticController < ApplicationController
   def index
-    @present_stations = Station.today_visits
-
-    unless Session.today.empty?
-      first_in_hash = {
-        name:  Session.today.first.station.name,
-        arrival_time:  Session.today.first.start
-      }
-    else
-      first_in_hash = {
-        name: "N/D",
-        arrival_time: "N/D"
-      }
-    end
-    @first_in = first_in_hash
-
-    unless Session.closed.today.empty?
-      unless Station.present.include?(Session.closed.today.last.station)
-        last_out_hash = {
-          name: Session.closed.today.last.station.name,
-          leaving_time: Session.closed.today.last.end
-        }
-      else
-        last_out_hash = {
-          name: Session.closed.today.last(2).first.station.name,
-          leaving_time: Session.closed.today.last.end
-        }
-      end
-    else
-      last_out_hash = {
-        name: "N/D",
-        leaving_time: "N/D"
-      }
-    end
-    @last_out = last_out_hash
-
-    @total_visits = Session.today.group(:station_id)
+    # @present_stations = Station.today_visits
+    #
+    # unless Session.today.empty?
+    #   first_in_hash = {
+    #     name:  Session.today.first.station.name,
+    #     arrival_time:  Session.today.first.start
+    #   }
+    # else
+    #   first_in_hash = {
+    #     name: "N/D",
+    #     arrival_time: "N/D"
+    #   }
+    # end
+    # @first_in = first_in_hash
+    #
+    # unless Session.closed.today.empty?
+    #   unless Station.present.include?(Session.closed.today.last.station)
+    #     last_out_hash = {
+    #       name: Session.closed.today.last.station.name,
+    #       leaving_time: Session.closed.today.last.end
+    #     }
+    #   else
+    #     last_out_hash = {
+    #       name: Session.closed.today.last(2).first.station.name,
+    #       leaving_time: Session.closed.today.last.end
+    #     }
+    #   end
+    # else
+    #   last_out_hash = {
+    #     name: "N/D",
+    #     leaving_time: "N/D"
+    #   }
+    # end
+    # @last_out = last_out_hash
+    #
+    # @total_visits = Session.today.group(:station_id)
 
   end
 
