97a30a24ecf7ec49d514439fa72917c83f8b17fe
ing.norante <eppedema@gmail.com>
2017-06-07 18:01:23 +0200

    Fix sock location for Puma

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,8 @@ rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://#{shared_dir}/tmp/sockets/guardiadiporta-puma.sock"
+
 
 # Logging
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
