ed09be7dbca984c6b234884ddd347f72ed8a1194
ing.norante <eppedema@gmail.com>
2017-06-07 17:38:08 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,6 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
 ## Defaults:
