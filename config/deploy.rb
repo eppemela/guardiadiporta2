9f0990a7128c5e8069f4582fe0835d81fd974302
ing.norante <eppedema@gmail.com>
2017-06-07 19:12:42 +0200

    Fix assets 404s in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :rbenv_roles, :all # default value
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml}
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
