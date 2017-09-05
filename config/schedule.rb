b97e61b3dfdcf01378bfb8a7bf874ff7090fdd9d
ing.norante <eppedema@gmail.com>
2017-09-05 23:54:50 +0200

    Fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
-ENV['DISPLAY'] = ":1.0"
-env 'DISPLAY', ':1.0'
+ENV['DISPLAY'] = ":0.0"
+env 'DISPLAY', ':0.0'
 
 every 3.minutes do
   rake "stations:update"
